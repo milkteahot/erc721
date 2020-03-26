ae5063605f2be1f088614c84b0b92f351cdaf9ce
milkteahot <greenegoode11@gmail.com>
2020-03-27 05:04:34 +0900

    함수 호출, 상태변수 참조

--- app/src/MainComponent.js
+++ app/src/MainComponent.js
@@ -1,24 +1,43 @@
 import React, {Component} from 'react';
-// import logo from "../public/logo.png";
+import logo from "./logo.png";
+import { PropTypes } from 'prop-types';
 
 class MainComponent extends Component {
-    constructor(props) {
+
+    constructor(props, context) {
         super(props);
+        this.contracts = context.drizzle.contracts;
+        this.deedToken = this.contracts.DeedToken;
+    }
+
+    componentDidMount() {
+        this.dataKey = this.deedToken.methods.totalSupply.cacheCall();
+    }
+
+    handleClick = () => {
+        this.totalSupply = this.props.DeedToken.totalSupply[this.dataKey].value;
     }
 
     render() {
         return (
             <div className="container">
+                <img src={logo} alt="drizzle-logo"/>
                 <h2>Account</h2>
                 {this.props.accounts[0]}
                 <h2>Balance</h2>
-                {/* {this.props.accountBalance[this.props.accounts[0]]} */}
+                {this.props.accountBalances[this.props.accounts[0]]}
                 <h2>DeedToken instance</h2>
                 {this.props.DeedToken.initialized && "True"}
-                
+                <h2>Total Supply</h2>
+                <p>{this.totalSupply}</p>
+                <button onClick={this.handleClick}>Total Supply</button>
             </div>
         )
     }
+}
+
+MainComponent.contextTypes = {
+    drizzle: PropTypes.object
 }
 
 export default MainComponent;
